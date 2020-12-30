97caec2375017c2c8038a43f45ecd2104435a8e8
Christopher Vouga <crvouga@gmail.com>
2020-12-30 15:36:29 -0700

    use square for better screen shot

--- src/match-three/board.js
+++ src/match-three/board.js
@@ -15,7 +15,7 @@ export const createRandomItem = () => ({
   color: randomNth(COLORS),
 });
 
-export const createRandomBoard = (rowCount = 6, columnCount = 7) =>
+export const createRandomBoard = (rowCount = 7, columnCount = 7) =>
   R.times(() => R.times(() => createRandomItem(), columnCount), rowCount);
 
 /* 
