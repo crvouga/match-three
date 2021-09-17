c73f48e424734ee5322848f0a568137283e7f089
Christopher Vouga <crvouga@gmail.com>
2021-09-16 21:56:56 -0700

    Updates

---
+++ precache-manifest.048316861d11a6c25932679cd111d61d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce3fb7b12a31d95371546e35b88ebc3",
+    "url": "/match-three/index.html"
+  },
+  {
+    "revision": "0107ca937232d981eec6",
+    "url": "/match-three/static/js/2.3546b652.chunk.js"
+  },
+  {
+    "revision": "b384649eb9470082f3e8048f3a066244",
+    "url": "/match-three/static/js/2.3546b652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0afb8ee98450b433f60",
+    "url": "/match-three/static/js/main.fa498be9.chunk.js"
+  },
+  {
+    "revision": "8316e45ab1ab17b56a50",
+    "url": "/match-three/static/js/runtime-main.c82ae0a7.js"
+  }
+]);
